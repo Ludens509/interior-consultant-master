d810268df50f129a1bba320083270af617d69e15
Ludens509 <alexandreludens2@gmail.com>
2022-01-29 18:22:07 -0500

    devChallenge files

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function toggleLogo(){
+    var logo = document.getElementById("logo");
+   if(logo.style.visibility === "hidden"){
+        logo.style.visibility = "visible";
+   }
+   else{
+       logo.style.visibility = "hidden";
+   }
+}
